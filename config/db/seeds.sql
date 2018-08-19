d4155fcee2d4b17eb6e4b5ae2a922eee72c511ed
rspears417 <racheal.spears417@gmail.com>
2018-08-18 23:20:04 -0500

    initial

---
+++ config/db/seeds.sql
@@ -0,0 +1,6 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Double-double');
+INSERT INTO burgers (burger_name) VALUES ('Vegan burger with cheese');
+INSERT INTO burgers (burger_name) VALUES ('Fish burger with tartar sauce');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
